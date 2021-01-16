541f5a8e33aa6346d848d46717825cd1f0bf35e0
TomaszBeben <tbeben@st.swps.edu.pl>
2021-01-16 18:03:17 +0100

    another

--- src/components/MessageForm/index.js
+++ src/components/MessageForm/index.js
@@ -1,22 +1,25 @@
 import React from 'react'
+import './index.css'
 
 const MessageForm = ({ handleSubmit, handleContentChange, message, user, handleUserChange }) => {
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
+        <div >
+            <form className='form' onSubmit={handleSubmit}>
                 <input
+                    className='name'
                     type='text'
                     name='user'
                     placeholder='name'
                     value={user}
                     onChange={(event) => handleUserChange(event.target.value)}/>
                 <input
+                    className='content'
                     placeholder='message'
                     type='text'
                     name='content'
                     value={message}
                     onChange={(event) => handleContentChange(event.target.value)} />
-                <input type='submit' value='Submit' />
+                <input className='button' type='submit' value='Send' />
             </form>
         </div>
     )
